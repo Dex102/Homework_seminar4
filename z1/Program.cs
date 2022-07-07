d35f0aadb9eeb3f7fb7d9da35ebaeff80bd9a7e7
EgorEvtushenko <ljvbyj0990@gmail.com>
2022-07-07 13:10:14 +0500

    Homework seminar 4

---
+++ z1/Program.cs
@@ -0,0 +1,18 @@
+﻿int SumNumbers (int N) 
+{
+    int sum = 0;
+    while (N > 0) 
+    {
+        sum += N % 10;
+        N = N / 10;
+    }
+    
+    return sum;
+}
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sum of numbers is " + SumNumbers(number));
+
+
---
+++ z2/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] NewArray(int size)
+{
+    int[] Array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = new Random().Next(1, 1000);
+    }
+
+    return Array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.Write("Input size array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] MyArray = NewArray(size);
+ShowArray(MyArray);
+
